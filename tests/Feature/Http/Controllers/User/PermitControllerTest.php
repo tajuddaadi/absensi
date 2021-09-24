ea220ff3e8c6b629565b2e2c5ffade9919955b74
Tajudda <tajudda@gmail.com>
2021-09-24 11:01:24 +0700

    add permit test

--- tests/Feature/Http/Controllers/User/PermitControllerTest.php
+++ tests/Feature/Http/Controllers/User/PermitControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Faker\Generator as Faker;
 
 class PermitControllerTest extends TestCase
 {
@@ -13,10 +14,18 @@ class PermitControllerTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testPermitData()
     {
-        $response = $this->get('/');
+        $user = factory(Permit::class)->create();
 
-        $response->assertStatus(200);
+        $response = $this->actingAs($user, 'api')
+            ->getJson('/api/permit-all');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'data' => true,
+            ]);
     }
 }
